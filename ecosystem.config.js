d973d0e6a3def9e86a726b2439e7ddd0bad50a7e
Jose Gutierrez <jigutierrez3@uc.cl>
2024-08-02 00:30:45 -0400

    pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Beep Server',
+      script: './build/index.js', // Replace with the path to your main file
+      instances: 1, // Or a specific number of instances
+      exec_mode: 'cluster', // For load balancing between instances
+      env: {
+        NODE_ENV: 'production',
+      },
+      env_development: {
+        NODE_ENV: 'development',
+      }
+    }
+  ]
+};
