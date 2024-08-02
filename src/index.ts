b0cd73ea975db6af0274c4831b25bf7470227889
Jose Gutierrez <jigutierrez3@uc.cl>
2024-08-02 01:41:25 -0400

    error logging

--- src/index.ts
+++ src/index.ts
@@ -54,6 +54,11 @@ app.listen(PORT, () => {
   console.log('Server is running on port', PORT);
 });
 
+process.on('uncaughtException', (err) => {
+  console.error('There was an uncaught error', err);
+  process.exit(1);
+});
+
 const shutdown = () => {
   console.log('Shutting down server');
   app.close();
