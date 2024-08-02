2c81dce306755777bc73a79ff87a28b6959cb4a3
Jose Gutierrez <jigutierrez3@uc.cl>
2024-08-02 01:52:25 -0400

    change config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: 'Beep Server',
+      name: 'beep',
       script: './build/src/index.js', // Replace with the path to your main file
       instances: 1, // Or a specific number of instances
-      exec_mode: 'cluster', // For load balancing between instances
       env: {
         NODE_ENV: 'production',
         PORT: '3000',
